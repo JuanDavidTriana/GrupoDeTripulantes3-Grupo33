de75abea32428768ed18200c07b1395528f008f0
victor <vialma678@gmail.com>
2021-10-11 12:52:07 -0500

    Integración bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -3,4 +3,5 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import "semantic-ui-css/semantic.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 createApp(App).use(store).use(router).mount('#app')
